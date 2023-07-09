44191a1053d7b10781b604e07d0a9c2751b656c3
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-07-09 10:44:47 +0100

    WIP: Resolved error build

---
+++ src/pages/blog.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from "@/layouts/DefaultLayout";
+import React from "react";
+
+export default function blog() {
+  return (
+    <DefaultLayout>
+      <h1 className="text-5xl text-center my-96">No Blog Post Yet!!!</h1>
+    </DefaultLayout>
+  );
+}
--- src/pages/blog/blog.types.tsx
+++
@@ -1,9 +0,0 @@
-export type BlogProps = {
-  blogPosts: {
-    slug: string;
-    title: string;
-    author: string;
-    date: string;
-  }[];
-  post: any;
-};
